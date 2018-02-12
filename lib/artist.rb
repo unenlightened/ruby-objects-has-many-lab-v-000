287bf2a1c4deece54d9113714a0174bb387a7a81
Chely Ho <adept_inept@yahoo.com>
2018-02-12 01:17:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name, :songs
-  @@song_count
-  
+  @@song_count = 0
+
   def initialize(name)
     @name = name
     @songs = []
@@ -10,6 +10,7 @@ class Artist
  def add_song(song)
    @songs << song
    song.artist = self
+   
  end
 
  def add_song_by_name(song)
