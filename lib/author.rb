5bf66c2f945ac1371ee07ccc880b44b97773cc1e
Chely Ho <adept_inept@yahoo.com>
2018-02-12 01:27:10 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,6 +9,7 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
@@ -17,5 +18,6 @@ class Author
   end
 
   def self.post_count
+    @@post_count
   end
 end
